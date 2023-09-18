c10a51c444c36b67e35d53fce9f4593bfae11326
Kinga Mikołajczyk <kinga.mikola@gmail.com>
2023-09-18 19:44:56 +0200

    refactor: persist-lib

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,31 @@
-import { configureStore } from '@reduxjs/toolkit';
-
+import {
+  configureStore,
+  combineReducers,
+  getDefaultMiddleware,
+} from '@reduxjs/toolkit';
 import { contactsReducer, filterReducer } from './slices';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const rootReducer = combineReducers({
+  contacts: contactsReducer,
+  filter: filterReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    contacts: contactsReducer,
-    filter: filterReducer,
-  },
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: ['persist/PERSIST'],
+    },
+  }),
 });
+
+export const persistor = persistStore(store);
